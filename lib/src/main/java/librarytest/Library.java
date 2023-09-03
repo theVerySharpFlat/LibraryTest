b97ffe97bd7013e7fa425faf51414004ea41be59
theVerySharpFlat <aidenramen29@gmail.com>
2023-09-03 15:00:30 -0500

    add wpi plugins to make the build succeed

--- lib/src/main/java/librarytest/Library.java
+++ lib/src/main/java/librarytest/Library.java
@@ -3,7 +3,14 @@
  */
 package librarytest;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+
 public class Library {
+    public Command giveMeACommandThatDoesSomething() {
+        return new PrintCommand("If you see this, the library probably works");
+    }
+
     public boolean someLibraryMethod() {
         return true;
     }
